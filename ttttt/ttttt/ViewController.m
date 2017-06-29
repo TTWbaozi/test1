7f9798a6feb4e250a841288dbf5d95260d54bf7b
tanqinwen <tangqinweng@163.com>
2017-06-29 09:54:56 +0800

    我也不知道说什么2222551212121212

---
+++ ttttt/ttttt/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  ttttt
+//
+//  Created by 谭覃文 on 2017/6/29.
+//  Copyright © 2017年 青橙软件. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
